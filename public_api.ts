902a17ebe6d2896dcd4dabc47229f0afd07648b2
ac899341@gmail.com <vpadmin@CHC.com>
2018-06-29 12:25:22 +0530

    Init

--- public_api.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/app/mc-login/mc-login.module';
-export * from './src/app/mc-signup/mc-signup.module';
-export * from './src/app/mc-consent/mc-consent.module';
-export * from './src/app/mc-profile/mc-profile.module';
-export * from './src/app/mc-widget-root/mc-widget-root.module';
